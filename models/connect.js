9eaaaecc6a76e098a53fd0fcf5d018bf99eec142
Louis B <louisbiret@gmail.com>
2016-07-22 17:57:23 -0400

    Fix merge conflicts

--- models/connect.js
+++ models/connect.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
 module.exports = "mongodb://pam:pam@ds023495.mlab.com:23495/pampam"
-=======
-module.exports = {
-    MONGODB_URI:"mongodb://pam:pam@ds023495.mlab.com:23495/pampam",
-}
->>>>>>> origin/master
