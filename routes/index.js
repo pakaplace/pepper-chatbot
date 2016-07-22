06f8a9e653f07a8acf68b458fb7cdb81bb031444
Louis B <louisbiret@gmail.com>
2016-07-22 11:51:14 -0400

    testing reflections

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+//** WIT REQUIRE **//
+let Wit = null;
+let log = null;
+
+try {
+  Wit = require('../').Wit;
+  log = require('node-wit').log;
+} catch(e){
+  Wit = require('node-wit').Wit
+  log = require('node-wit').log;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
