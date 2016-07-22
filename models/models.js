9ae30a569fbbd7dcb57e97fe206dc5d79d534bea
Louis B <louisbiret@gmail.com>
2016-07-22 12:20:05 -0400

    Timeline displaying

--- models/models.js
+++ models/models.js
@@ -3,7 +3,12 @@ var findOrCreate = require('mongoose-findorcreate')
 
 // Create a connect.js inside the models/ directory that
 // exports your MongoDB URI!
-var connect = process.env.MONGODB_URI || require('./connect');
+var connect = require('./connect.js');
+
+// var db = mongoose.connection;
+// db.once('open', function callback () {
+// console.log("DB Connected!");
+// });
 
 // If you're getting an error here, it's probably because
 // your connect string is not defined or incorrect.
@@ -15,19 +20,88 @@ var userSchema = mongoose.Schema({
     required: true,
     default: 0
   },
+  // token: {
+  //   type: Number
+  // },
   facebookId: { //store facebook id here
     type: String,
     required: true
   },
-  routine:{
+  routine:[
+    {
+      routine: String,
+      duration:Number
+    }
+  ],
+  routineCopy:{
     type: Array
   },
-  timeToWakeUP: {
+  city: {
     type: String
   },
-  toDo:{
-    type: Array,
-  }
+  timeToWakeUp: [
+    {
+      time: String,
+      hour: Number,
+      minute:Number
+    }
+  ],
+  tasks:{
+    type: Array
+  }, 
+  prevState: {
+    type: Number
+  },
+  firstname:{
+    type: String
+  },
+  timezone:{
+    type: Number
+  },
+  locale:{
+    type:String
+  },
+  gender:{
+    type:String
+  },
+  missingDuration:{
+    type: String,
+    name: "Routine but no duration"
+  },
+  missingRoutine:{
+    type: String,
+    name: "Duration but no routine"
+  },
+  reflectionTime: [
+    {
+      hour: {
+        type: Number,
+        default: 21
+      },
+      minute: {
+        type: Number,
+        default: 0
+      }
+    }
+  ],
+  reflectionQuestion: {
+    type: String
+  },
+  reflection:{
+    "title": {
+        // "media": {
+        //   "url": String,
+        //   "caption": String,
+        //   "credit": String
+        // },
+        "text": {
+          "headline": String,
+          "text": String
+        }
+    },
+    "events": []
+    }
 });
+
 userSchema.plugin(findOrCreate);
 module.exports = {User: mongoose.model('User', userSchema)};
